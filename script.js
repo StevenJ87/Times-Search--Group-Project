0710544d3883366c746e2e928a28d763d2a26a17
Ayodeji Amosun <ayodeji.amosun04@gmail.com>
2020-06-26 14:14:20 -0500

    adding variables

--- script.js
+++ script.js
@@ -1,22 +1,24 @@
 $("searchBtn").on("click", function (event) {
     event.preventDefault();
 
-    var question = $("search-term").val();
+    var question = $("#").val();
+    var recordNum = $("#").val();
+    var startYear = $("").val();
+    var endYear = $("").val();
 
+    var apiKey = "Uz7yxjvAafqF73Jew9sNdtBGMc8MEE9z";
+    var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + question "&api-key=" + apiKey 
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    })
+        .then(function(response) {
+            console.log(response);
+        })
 })
 
 
-var apiKey = "Uz7yxjvAafqF73Jew9sNdtBGMc8MEE9z";
-var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + question "&api-key=" + apiKey 
 
 
-
-$.ajax({
-    url: queryURL,
-    method: "GET"
-  })
-    .then(function(response) {
-        console.log(response);
-    })
-
-var headline = response.docs[0].headline.main;
+// var headline = response.docs[0].headline.main;
