379cd9fdd6a5eac3633b841626f2fd5d8a0df9fa
Ayodeji Amosun <ayodeji.amosun04@gmail.com>
2020-06-26 14:04:42 -0500

    starting stages of javascript

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
+$("searchBtn").on("click", function (event) {
+    event.preventDefault();
+
+    var question = $("search-term").val();
+
+})
+
+
 var apiKey = "Uz7yxjvAafqF73Jew9sNdtBGMc8MEE9z";
-var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=" + apiKey 
+var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + question "&api-key=" + apiKey 
 
 
+
+$.ajax({
+    url: queryURL,
+    method: "GET"
+  })
+    .then(function(response) {
+        console.log(response);
+    })
+
+var headline = response.docs[0].headline.main;
